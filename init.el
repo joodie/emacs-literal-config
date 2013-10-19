de084f63d1f70a8d30d49db1fdd29d023bd65fda
Joost Diepenmaat <joost@zeekat.nl>
2013-10-19 23:23:30 +0200

    Add theme.

--- init.el
+++ init.el
@@ -2,4 +2,19 @@
 (cask-initialize)
 (require 'pallet)
 (require 'org)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("8b6fcbe834eb12bc1e1e7d3e26568457004a0a9daf95def74a94d855af90f2f1" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
 (org-babel-load-file "~/.emacs.d/emacs.org")
+
