2acfa25dbe5dcdc2071cf85e4c626bfa0dbf1697
Joost Diepenmaat <joost@zeekat.nl>
2013-10-22 09:25:11 +0200

    Check that Org is up-to-date at startup.

--- init.el
+++ init.el
@@ -1,5 +1,17 @@
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
-(require 'pallet)
+
+(when (not (cl-remove-if-not 
+	    (lambda (p) (equal 'org (car p)))
+	    package-alist))
+  (message "No org-mode package found; installing now...")
+  (package-install 'org))
+
 (require 'org)
+(when (string-match "^[1234567]" (org-version))
+  (warn "Org-Mode is out of date. We expect org 8 or higher, but instead we have %s" (org-version)))
+
+(require 'pallet)
+
 (org-babel-load-file "~/.emacs.d/emacs.org")
+
