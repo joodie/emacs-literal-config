4c379c9eee616c8fec8f65ad515387b0fa259599
Joost Diepenmaat <joost@zeekat.nl>
2014-08-22 09:01:20 +0200

    Dependencies

---
+++ ext/ido-yes-or-no.el
@@ -0,0 +1,32 @@
+;;; ido-yes-or-no.el --- Use Ido to answer yes-or-no questions
+
+;; Copyright 2011 Ryan C. Thompson
+
+;; Author: Ryan C. Thompson
+;; URL: https://github.com/DarwinAwardWinner/ido-yes-or-no
+;; Version: 1.2
+;; Package-Requires: ()
+
+(require 'ido)
+
+;;;###autoload
+(define-minor-mode ido-yes-or-no-mode
+  "Use ido for `yes-or-no-p'."
+  nil
+  :global t
+  :group 'ido)
+
+(defun ido-yes-or-no-p (prompt)
+  "Ask user a yes-or-no question using ido."
+  (let* ((yes-or-no-prompt (concat prompt " "))
+         (choices '("yes" "no"))
+         (answer (ido-completing-read yes-or-no-prompt choices nil 'require-match)))
+    (string= answer "yes")))
+
+(defadvice yes-or-no-p (around use-ido activate)
+  (if ido-yes-or-no-mode
+      (setq ad-return-value (ido-yes-or-no-p prompt))
+    ad-do-it))
+
+(provide 'ido-yes-or-no)
+;;; ido-yes-or-no.el ends here
