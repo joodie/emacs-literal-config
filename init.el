13d26bc4dbebf620ab964eaa0073a90f8e9bdec9
Heikki Lehvaslaiho <heikki.lehvaslaiho@gmail.com>
2015-04-08 15:54:45 +0200

    init.el: reorganize to start using use-package
    first, install load and initialize cask, then pallet. Then, use-package.
    Lastly, load the org-file
    
    Also, remove sanity code for org-version < 8.

--- init.el
+++ init.el
@@ -1,41 +1,39 @@
 ;; start recording loading time
 (defconst emacs-start-time (current-time))
 
-(require 'use-package)
-;;(require 'bind-key)
-;;(setq use-package-verbose t)
-
-;; standalone cask
+;; cask and pallet
+;;standalone cask
 ;;(require 'cask "~/.cask/cask.el")
 ;; OSX: cask from homebrew
 (require 'cask "/usr/local/share/emacs/site-lisp/cask.el")
 (cask-initialize)
-
-(when (not (cl-remove-if-not
-	    (lambda (p) (equal 'org (car p)))
-	    package-alist))
-  (message "No org-mode package found; installing now...")
-  (package-install 'org))
-
-(require 'org)
-(when (string-match "^[1234567]" (org-version))
-  (warn "Org-Mode is out of date. We expect org 8 or higher, but instead we have %s" (org-version)))
-
 (require 'pallet)
 (pallet-mode t)
 
+(let ((elapsed
+       (float-time
+        (time-subtract (current-time) emacs-start-time))))
+  (message "Loading %s...done (%.3fs)" "cask&pallet" elapsed))
+
+;; use-package
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish)                ;; if you use :diminish
+(require 'bind-key)                ;; if you use any :bind variant
+
+;;load the main configuration
 (org-babel-load-file "~/.emacs.d/emacs.org")
 
-
 ;; report time
-(let ((elapsed (float-time (time-subtract (current-time)
-                                          emacs-start-time))))
+(let ((elapsed
+       (float-time
+        (time-subtract (current-time) emacs-start-time))))
   (message "Loading %s...done (%.3fs)" load-file-name elapsed))
 
 (add-hook 'after-init-hook
           `(lambda ()
-             (let ((elapsed (float-time (time-subtract (current-time)
-                                                       emacs-start-time))))
+             (let ((elapsed
+                    (float-time (time-subtract (current-time) emacs-start-time))))
                (message "Loading %s...done (%.3fs) [after-init]",
                         load-file-name elapsed)))
           t)
