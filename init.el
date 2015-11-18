97e178023ef1600937972d2048f139c226b33ab6
Joost Diepenmaat <joost@zeekat.nl>
2015-11-18 20:00:46 +0100

    Add auto-generated stuff from package-install

--- init.el
+++ init.el
@@ -1,3 +1,10 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
 
