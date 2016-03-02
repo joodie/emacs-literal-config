e494b0e0f1b5b3d79335ad5b9634fb5e4bff0373
Joost Diepenmaat <joost@zeekat.nl>
2016-03-02 16:00:23 +0100

    Auto install from melpa-stable

--- init.el
+++ init.el
@@ -5,8 +5,10 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
-(require 'cask "~/.cask/cask.el")
-(cask-initialize)
+(add-to-list 'package-archives
+	     '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
+
+(require 'cl)
 
 (when (not (cl-remove-if-not 
 	    (lambda (p) (equal 'org (car p)))
@@ -14,11 +16,84 @@
   (message "No org-mode package found; installing now...")
   (package-install 'org))
 
+(setq my/package-list
+      '(aggressive-indent
+	auctex
+	auto-complete
+	cider
+	circe
+	clj-refactor
+	clojure-mode
+	coffee-mode
+	company
+	csv-mode
+	dash
+        diminish
+	exec-path-from-shell
+	flycheck
+	fringe-helper
+	git
+	git-gutter
+	git-gutter-fringe
+	graphviz-dot-mode
+	haml-mode
+	highlight-parentheses
+        ;;	htmlize
+	inf-ruby
+        flycheck
+	inflections
+	js2-mode
+	json-mode
+	json-reformat
+	less-css-mode
+        ;;	leuven-theme
+	magit
+	makey
+	markdown-mode
+	multiple-cursors
+	names
+	neotree
+	paredit
+	pkg-info
+	popup
+	powerline
+        ;;	pretty-symbols
+	projectile
+	rainbow-delimiters
+	rainbow-mode
+	rspec-mode
+	ruby-compilation
+	rvm
+	s
+	sass-mode
+	shut-up
+	slamhound
+	smart-mode-line
+	smartparens
+	smex
+	tracking
+	undo-tree
+	nyan-mode
+	writeroom-mode
+	yaml-mode
+	yasnippet
+	))
+
+
+;; fetch the list of packages available
+(unless (file-exists-p package-user-dir)
+  (package-refresh-contents))
+
+;; install the missing packages
+(dolist (package my/package-list)
+  (unless (package-installed-p package)
+    (message "Installing %s" package)
+    (package-install package)))
+
+
 (require 'org)
 (when (string-match "^[1234567]" (org-version))
   (warn "Org-Mode is out of date. We expect org 8 or higher, but instead we have %s" (org-version)))
-
-(require 'pallet)
 
 (org-babel-load-file "~/.emacs.d/emacs.org")
 
