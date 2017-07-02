041102d595a780da5c5e4d7fcf046c430d51d9ae
Heikki Lehvaslaiho <heikki.lehvaslaiho@gmail.com>
2017-07-02 16:16:35 +0300

    Massive reorganisation
    Blogged here:
    
    Efficient Emacs.org ➞ .el tangling
    Holger's Computer Calisthenics and Orthodontia
    http://www.holgerschurig.de/en/emacs-init-tangle/
    
    and expanded here:
    
    https://bitbucket.org/holgerschurig/emacsconf/src
    
    The native org mode tangling is replaced by custom elisp code that is
    blazingly fast. There is less code in the actual org file and the
    order of tangled code is same as in the org file.

--- init.el
+++ init.el
@@ -6,7 +6,6 @@
 ;; Set loading of newer files over compiled at the very beginning
 (setq load-prefer-newer t)
 
-
 (require 'package)
 (setq package-enable-at-startup nil)
 (package-initialize t)
@@ -45,8 +44,67 @@
 (require 'cl)
 (setq use-package-always-ensure t) ;; no need for :ensure key
 
+
 ;;load the main configuration
-(org-babel-load-file "~/.emacs.d/emacs.org")
+;;(org-babel-load-file "~/.emacs.d/emacs.org")
+
+
+(defun my-tangle-section-canceled ()
+  "Checks if the previous section header was PEND"
+  (save-excursion
+    (if (re-search-backward "^\\*+\\s-+\\(.*?\\)?\\s-*$" nil t)
+        (progn
+          ;; (message "FOUND '%s'" (match-string 1))
+          (string-prefix-p "PEND" (match-string 1)))
+      nil)))
+
+(defun my-tangle-config-org (orgfile elfile)
+  "This function will write all source blocks from ORGFILE into
+ELFILE that are ...
+
+- not marked as :tangle no
+- have a source-code of =emacs-lisp=
+- doesn't have the todo-marker PEND (for PENDING)"
+
+  (let* ((body-list ())
+         (gc-cons-threshold most-positive-fixnum)
+         (org-babel-src-block-regexp   (concat
+                                        ;; (1) indentation                 (2) lang
+                                        "^\\([ \t]*\\)#\\+begin_src[ \t]+\\([^ \f\t\n\r\v]+\\)[ \t]*"
+                                        ;; (3) switches
+                                        "\\([^\":\n]*\"[^\"\n*]*\"[^\":\n]*\\|[^\":\n]*\\)"
+                                        ;; (4) header arguments
+                                        "\\([^\n]*\\)\n"
+                                        ;; (5) body
+                                        "\\([^\000]*?\n\\)??[ \t]*#\\+end_src")))
+    (with-temp-buffer
+      (insert-file-contents orgfile)
+      (goto-char (point-min))
+      (while (re-search-forward org-babel-src-block-regexp nil t)
+        (let ((lang (match-string 2))
+              (args (match-string 4))
+              (body (match-string 5))
+              (canc (my-tangle-section-canceled)))
+          (when (and (string= lang "emacs-lisp")
+                     (not (string-match-p ":tangle\\s-+no" args))
+                     (not canc))
+            (add-to-list 'body-list body)))))
+    (with-temp-file elfile
+      (insert ";; *- lexical-binding: t; -*-\n")
+      (insert (format ";; Don't edit this file, edit %s instead ...\n\n" orgfile))
+      ;; (insert (apply 'concat (reverse body-list)))
+      (apply 'insert (reverse body-list)))
+    (message "Wrote %s ..." elfile)
+    (byte-compile-file elfile)))
+
+(let ((orgfile (concat user-emacs-directory "emacs.org"))
+      (elfile (concat user-emacs-directory "emacs.el"))
+      (gc-cons-threshold most-positive-fixnum))
+  (when (or (not (file-exists-p elfile))
+            (file-newer-than-file-p orgfile elfile))
+    (my-tangle-config-org orgfile elfile))
+  (load-file elfile))
+
 
 ;; report time
 (let ((elapsed
