3157defe2c7478af437d3984900ddb4aa0171dca
Heikki Lehvaslaiho <heikki.lehvaslaiho@gmail.com>
2018-04-29 12:59:01 +0300

    custom.el: empty

--- custom.el
+++
@@ -1,7 +0,0 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain och instance.
- ;; If there is more than one, they won't work right.
-
-)
